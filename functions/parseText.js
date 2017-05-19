d0e082e3c331d38fa6f0ebd5673d69952e289293
Botseer <homeu99@gmail.com>
2017-05-19 17:17:57 +0545

    Update parseText.js

--- functions/parseText.js
+++ functions/parseText.js
@@ -1,7 +1,7 @@
 /*
 	Yes, this is a modified version of Auto by Hackzilla
 */
-const issue = require('./issueList.json');
+const issue = require(path.join(__dirname, 'issueList.json'));
 function parse(input) {
   if (input.includes('Block-scoped declarations (let, const, function, class) not yet supported outside strict mode')) {
     return {
