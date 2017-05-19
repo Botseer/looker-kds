1fde5b0ff3b8b9343b5c9bb7a82e20d647349964
The_Learner <bsantosh909@gmail.com>
2017-05-19 17:51:29 +0545

    Update channelDelete.js

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,5 +1,5 @@
 module.exports = async (channel) => {
 
-channel.guild.channels.get(`313617748422950912`).send(`**${channel.type}** channel named __**${channel.name}**__ has been removed from this server.`).catch(err => console.error(err));
+channel.guild.channels.get(`304930941174939649`).send(`**${channel.type}** channel named __**${channel.name}**__ has been removed from this server.`).catch(err => console.error(err));
 
 };
