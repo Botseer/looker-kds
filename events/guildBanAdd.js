b4e1b46c84f785424c7f2069a980a133c4abdd6a
The_Learner <bsantosh909@gmail.com>
2017-05-19 17:52:00 +0545

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,5 +1,5 @@
 module.exports = async (guild, user) => {
 
-channel.guild.channels.get(`313617748422950912`).send(`**${user.tag}** has been banned from __**${guild.name}**__ !`).catch(err => console.error(err));
+channel.guild.channels.get(`304930941174939649`).send(`**${user.tag}** has been banned from __**${guild.name}**__ !`).catch(err => console.error(err));
 
 };
