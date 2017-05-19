8610f116d3c28341ab3a8441732ad02e442784fc
The_Learner <bsantosh909@gmail.com>
2017-05-19 17:51:58 +0545

    Update guildBanRemove.js

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -1,5 +1,5 @@
 module.exports = async (guild, user) => {
 
-channel.guild.channels.get(`313617748422950912`).send(`Ban for **${user.tag}** has been removed !`).catch(err => console.error(err));
+channel.guild.channels.get(`304930941174939649`).send(`Ban for **${user.tag}** has been removed !`).catch(err => console.error(err));
 
 };
