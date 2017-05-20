72cabcee3b43afd1a7bad89b598aa97372ce1da5
The_Learner <bsantosh909@gmail.com>
2017-05-20 08:20:18 +0545

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,21 @@
+const Discord = require('discord.js');
 const config = require('../config.json');
 exports.run = (client, message, args) => {
-  
-  if(message.channel.id !== "308184273100210176") return;
-  
+  client.commandsss = new Discord.Collection();
+
   let permlvl = 0;
   let mod_role = message.guild.roles.find('name', config.modRole);
   if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 1;
-  if (message.author.id === config.ownerId) permlvl = 2;
 
   if (!args[0]) {
-    client.commands = client.commands.filter(c => c.conf.permLevel <= permlvl)
-    const commandNames = Array.from(client.commands.keys());
+    client.commandsss = client.commands.filter(c => c.conf.permLevel <= permlvl)
+    const commandNames = Array.from(client.commandsss.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    message.channel.send(`= Command List =\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: "asciidoc"})
+    message.channel.send(`= Command List =\n\n[Use ${config.prefix}help <commandname> for details]\n\n${client.commandsss.map(c => `${config.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code: "asciidoc"})
   } else {
     let command = args[0];
-    if (client.commands.has(command)) {
-      command = client.commands.get(command);
+    if (client.commandsss.has(command)) {
+      command = client.commandsss.get(command);
       message.channel.send(`= ${command.help.name} = \n${command.help.description}\nUsage:: ${command.help.usage}`, {code: "asciidoc"});
     }
   }
