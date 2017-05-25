0137e6c614ae89efea156ba6ec780ea8eb564941
The_Learner <bsantosh909@gmail.com>
2017-05-25 14:29:54 +0545

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ const errorChecks = require('../functions/parseText.js');
 module.exports = async message => {
   let client = message.client;
   if (message.author.bot) return;
-  if ((message.content.toLowerCase().includes('https://discord.gg')) || (message.content.toLowerCase().includes('https://discord.me'))) {
+  if ((message.content.toLowerCase().includes('https://discord.gg')) || (message.content.toLowerCase().includes('https://discord.me')) || (message.content.toLowerCase().includes('discord.gg')) || (message.content.toLowerCase().includes('discord.me'))) {
     message.delete();
     message.reply("You cant promote your servers here!");
     message.guild.channels.get("305666620796174337").send(`<@${message.author.id}> tried promoting personal servers here.`);
