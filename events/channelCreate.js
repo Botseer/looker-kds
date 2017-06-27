800b6ed4bb9c0dd89301195b0bda6b2c742dc3cd
Botseer <homeu99@gmail.com>
2017-06-27 16:46:39 +0545

    Delete channelCreate.js

--- events/channelCreate.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (channel) => {
-return;
-channel.guild.channels.get(`308184273100210176`).send(`**${channel.createdAt}**:\nNew **${channel.type}** channel named __**${channel.name}**__ has been added to this server.`).catch(err => console.error(err));
-
-};
