138ddfb043f15b05ad84fc9c4ea96a81b2ef1f4e
Botseer <homeu99@gmail.com>
2017-06-27 16:47:23 +0545

    Delete guildBanRemove.js

--- events/guildBanRemove.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (guild, user) => {
-
-channel.guild.channels.get(`308184273100210176`).send(`Ban for **${user.tag}** has been removed !`).catch(err => console.error(err));
-
-};
