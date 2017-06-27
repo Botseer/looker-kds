7c232f177782dc47062ea237104bbc6dfe64961b
Botseer <homeu99@gmail.com>
2017-06-27 17:12:20 +0545

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,12 +7,11 @@ const embed = new Discord.RichEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL)
 .setColor(0x4499E7)
 .setDescription('\Hello everyone, This is the official server for the discussion about the Dyanmic Emerald hacks created by KDS and DizzyEgg!\nMore info about the server is in the channel #server-info-and-rules.')
-.addField('SERVER ID', message.guild.id, true)
-.addField('MEMBERS', message.guild.memberCount + " (online: " + message.guild.members.filter(m => ((m.user.presence.status === "online" )||(m.user.presence.status === "dnd" )||(m.user.presence.status === "idle" ))).size + " )", true)
-.addField('TEXT CHANNELS', message.guild.channels.filter(c => c.type === "text").size , true)
-.addField('VOICE CHANNELS', message.guild.channels.filter(c => c.type === "voice").size , true)
-.addField('ROLES', message.guild.roles.size, true)
-.addField('CREATED DATE', moment(message.guild.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), true)
+.addField('SERVER ID', message.guild.id, false)
+.addField('MEMBERS', message.guild.memberCount + " (online: " + message.guild.members.filter(m => ((m.user.presence.status === "online" )||(m.user.presence.status === "dnd" )||(m.user.presence.status === "idle" ))).size + " )", false)
+.addField('TEXT CHANNELS', message.guild.channels.filter(c => c.type === "text").size , false)
+.addField('VOICE CHANNELS', message.guild.channels.filter(c => c.type === "voice").size , false)
+.addField('CREATED DATE', moment(message.guild.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), false)
 
 message.channel.send({embed});
 }
