b2e4a09a6aa3a793479c5fc46ea2c80055e18160
Botseer <homeu99@gmail.com>
2017-06-27 16:47:04 +0545

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -4,8 +4,4 @@ module.exports = client => {
   client.on('message', reqEvent('message'));
   client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
   client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
-  client.on('channelCreate', reqEvent('channelCreate'));
-  client.on('channelDelete', reqEvent('channelDelete'));
-  client.on('guildBanAdd', reqEvent('guildBanAdd'));
-  client.on('guildBanRemove', reqEvent('guildBanRemove'));
 };
