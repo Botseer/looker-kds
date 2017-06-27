c73a1c6351690ade1647a7b0066c7334231fedd3
Botseer <homeu99@gmail.com>
2017-06-27 16:55:23 +0545

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,16 +2,15 @@ const Discord = require('discord.js');
 const moment = require('moment');
 exports.run = (client, message, args) => {
 
- if(message.channel.id !== "308184273100210176") return;
  
 const embed = new Discord.RichEmbed()
 .setAuthor(message.guild.name, message.guild.iconURL)
 .setColor(0x4499E7)
-.setDescription('\nHello everyone, This is a server for pokemon lovers and friendly users around.\nYou can share all your idea about pokemon, talk about rom-hacking and also suggest some improvements as well.')
+.setDescription('\Hello everyone, This is the official server for the discussion about the Dyanmic Emerald hacks created by KDS and DizzyEgg!\nMore info about the server is in the channel #server-info-and-rules.')
 .addField('SERVER ID', message.guild.id, true)
 .addField('OWNER', message.guild.owner.user.tag, true)
 .addField('MEMBERS', message.guild.memberCount + " (online: " + message.guild.members.filter(m => ((m.user.presence.status === "online" )||(m.user.presence.status === "dnd" )||(m.user.presence.status === "idle" ))).size + " )", true)
-.addField('TEXT CHANNELS', message.guild.channels.filter(c => c.type === "text").size-1 , true)
+.addField('TEXT CHANNELS', message.guild.channels.filter(c => c.type === "text").size , true)
 .addField('VOICE CHANNELS', message.guild.channels.filter(c => c.type === "voice").size , true)
 .addField('ROLES', message.guild.roles.size, true)
 .addField('CREATED DATE', moment(message.guild.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'), true)
