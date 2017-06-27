4154920e016d63b4fafbb38df6665c0a22e10926
Botseer <homeu99@gmail.com>
2017-06-27 16:46:47 +0545

    Delete channelDelete.js

--- events/channelDelete.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (channel) => {
-return;
-channel.guild.channels.get(`308184273100210176`).send(`**${channel.type}** channel named __**${channel.name}**__ has been removed from this server.`).catch(err => console.error(err));
-
-};
