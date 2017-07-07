ac2d0f49ca1c474fd2c1f7ae5eee24f4689e388c
The_Learner <bsantosh909@gmail.com>
2017-07-07 18:25:49 +0545

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 const moment = require('moment');
 exports.run = function(client, message, args) {
  
@@ -7,20 +8,19 @@ if (message.mentions.users.size === 0){
 }else{
 	tempn = message.mentions.users.first().id;
 }
-    console.log(tempn);
-	let ui_name = client.guilds.get(message.guild.id).members.get(tempn);
-    console.log(ui_name);
-	let spymsg = [
-		'USER INFO',
-		`User ID   : ${ui_name.user.id}`,
-		`Username  : ${ui_name.user.username}`,
-		`Nickname  : ${ui_name.nickname}`,
-		`Discrim   : #${ui_name.user.discriminator}`,
-		`Bot       : ${ui_name.user.bot}`,
-		`Joined    : ${moment(ui_name.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a')}`,
-		`Created   : ${moment(ui_name.user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a')}`
-	];
-    message.channel.send(spymsg).then(m => m.editCode('LDIF', spymsg).catch(error => console.log(error.stack)).catch(error => console.log(error.stack)))
+    let ui_name = client.guilds.get(message.guild.id).members.get(tempn);
+	
+    	const embed = new Discord.RichEmbed()
+		.setAuthor(ui_name.user.username, ui_name.user.avatarURL)
+		.setColor('RANDOM')
+		.addField('Username', ui_name.user.username, true)
+		.addField('Nickname', ui_name.nickname, true)
+		.addField('Created', moment(ui_name.user.createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a'))
+		.addField('Joined', moment(ui_name.joinedAt).format('dddd, MMMM Do YYYY, h:mm:ss a'))
+		.addField('Discrim', `#**${ui_name.user.discriminator}**`)
+		.addField('Status', ui_name.user.presence.status,true)
+		.addField('Bot', ui_name.user.bot,true)
+	message.channel.send({embed});
 };
 
 exports.conf = {
